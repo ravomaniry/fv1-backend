4f72c3c2ce3efae7d92ec08b9920890038d6f085
Ravo Maniry <arm@e2f.com>
2023-11-04 09:39:33 +0300

    start script command

---
+++ scripts/start-db.sh
@@ -0,0 +1,7 @@
+export $(cat .env | xargs);
+docker run -p 3306:3306 \
+    -e MYSQL_USER=$MYSQL_USER \
+    -e MYSQL_DATABASE=$MYSQL_DATABASE \
+    -e MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD \
+    -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
+    mysql:8.0.31
